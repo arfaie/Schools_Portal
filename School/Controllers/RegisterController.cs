2214dbd0de07e9e279b10941880ff5b5ab7d27dd
Amir Ghasemi <amirghasemi615@gmail.com>
2020-05-14 10:02:13 +0430

    working on RegisterController

--- School/Controllers/RegisterController.cs
+++ School/Controllers/RegisterController.cs
@@ -2,14 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using School.Models;
 
 namespace School.Controllers
 {
     public class RegisterController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public RegisterController(UserManager<ApplicationUser> userManager)
         {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var User = await _userManager.GetUserAsync(HttpContext.User);
+
+            if (User == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
             return View();
         }
     }
