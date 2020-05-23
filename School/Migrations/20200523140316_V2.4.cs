9acbebbe806148351a0b634b97555a1279eb8360
Amir Ghasemi <amirghasemi615@gmail.com>
2020-05-23 18:33:57 +0430

    .

---
+++ School/Migrations/20200523140316_V2.4.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace School.Migrations
+{
+    public partial class V24 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IdLevel",
+                table: "Costs",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Costs_IdLevel",
+                table: "Costs",
+                column: "IdLevel");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Costs_Levels_IdLevel",
+                table: "Costs",
+                column: "IdLevel",
+                principalTable: "Levels",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Costs_Levels_IdLevel",
+                table: "Costs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Costs_IdLevel",
+                table: "Costs");
+
+            migrationBuilder.DropColumn(
+                name: "IdLevel",
+                table: "Costs");
+        }
+    }
+}
